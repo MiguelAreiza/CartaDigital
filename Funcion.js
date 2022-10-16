f52d750b7b0e1fe05c8bb02494cbd2634b862f71
MiguelAreiza <115864255+MiguelAreiza@users.noreply.github.com>
2022-10-16 02:39:25 -0500

    Add files via upload

---
+++ Funcion.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    $('#btnIng').click(function() {
+        let lib = new google.translate.TranslateService();
+        lib.translatePage('es', 'en', function () {});
+        $("#btnIng")[0].style.display = 'none'
+        $("#btnEsp")[0].style.display = ''
+
+    })
+    $('#btnEsp').click(function() {
+        $("#btnEsp")[0].style.display = 'none'
+        $("#btnIng")[0].style.display = ''
+        location.reload()
+    })   
+
+});
+
