9f10d0643a2bdce50e4d4de0dc60731e63b012af
MiguelAreiza <115864255+MiguelAreiza@users.noreply.github.com>
2022-10-16 03:50:19 -0500

    Update Funcion.js

--- Funcion.js
+++ Funcion.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
     $('#btnIng').click(function() {
         let lib = new google.translate.TranslateService();
         lib.translatePage('es', 'en', function () {});
-        $("#btnIng")[0].style.display = 'none'
-        $("#btnEsp")[0].style.display = ''
+        $("#btnIng")[0].style.display = 'none';
+        $("#btnEsp")[0].style.display = '';
 
-    })
+    });
     $('#btnEsp').click(function() {
-        $("#btnEsp")[0].style.display = 'none'
-        $("#btnIng")[0].style.display = ''
-        location.reload()
-    })   
-
+        $("#btnEsp")[0].style.display = 'none';
+        $("#btnIng")[0].style.display = '';
+        location.reload();
+    });  
+    $('#btnWp').click(function(){
+        window.open('https://wa.link/5zwm9d');
+    });
+    
 });
 
